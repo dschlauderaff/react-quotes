5653dff15f389eff07d27d85e86c4764caa28de3
dschlauderaff <dschlaud@gmail.com>
2018-01-19 18:41:54 -0600

    creates App contaner component

---
+++ frontend/quotes/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.jsx";
+
+const quotes = document.querySelector("#quotes");
+ReactDOM.render(<App />, quotes);

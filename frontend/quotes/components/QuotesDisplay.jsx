894cbf6856a862f77873d7cb512bff7cc97a8832
dschlauderaff <dschlaud@gmail.com>
2018-01-23 21:06:35 -0600

    styles added, split quotesdisplay into 3 new components: quoteNavigation, Quotefooter, and quoteText

--- frontend/quotes/components/QuotesDisplay.jsx
+++ frontend/quotes/components/QuotesDisplay.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 import queryString from 'query-string'
 import axios from 'axios'
+import QuoteText from './QuoteText';
+import QuoteNavigation from './QuoteNavigation';
+import QuoteFooter from './QuoteFooter';
 
 class QuotesDisplay extends React.Component {
   constructor () {
     super()
     this.state = {
-      quote: {}
+      quote: {},
+      fireRedirect: false
     }
   }
 
@@ -18,6 +22,7 @@ class QuotesDisplay extends React.Component {
       })
       .catch(error => {
         console.error(error)
+        this.setState({ fireRedirect: true})
       })
   }
 
@@ -50,18 +55,23 @@ class QuotesDisplay extends React.Component {
 
     return (
       <div>
-        {previousQuoteId &&
-          <Link to={`/?quote=${previousQuoteId}`}>
-            Previous
-            </Link>
-          }
-          {nextQuoteId &&
-            <Link to={`/?quote=${nextQuoteId}`}>
-              Next
-              </Link>
+        <div className="quote-container">
+          {this.state.fireRedirect && 
+          <Redirect to={'/'} />
             }
-        <p>{this.state.quote.text}</p>
-        <p>{this.state.quote.author}</p>
+          {previousQuoteId &&
+          <QuoteNavigation direction='previous' otherQuoteId={previousQuoteId} />
+            }
+          <QuoteText quote={this.state.quote} />
+          
+          {nextQuoteId &&
+          <QuoteNavigation direction='next' otherQuoteId={nextQuoteId} />
+              }
+        </div>
+        
+        {this.state.quote.id !== parseInt(this.props.startingQuoteId, 10) &&
+        <QuoteFooter startingQuoteId={this.props.startingQuoteId} />
+        }
       </div>
     )
   }
